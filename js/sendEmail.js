98e8ffb6600d09488c24a0af6e705539add96f8a
KNN1244 <kaungnyin@cpp.edu>
2024-11-09 21:59:05 -0800

    Fixes and Added Project Section

---
+++ js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendEmail() {
+    var templateParams = {
+        name: document.querySelector('[name="name"]').value,
+        email: document.querySelector('[name="email"]').value,
+        subject: document.querySelector('[name="subject"]').value,
+        message: document.querySelector('[name="message"]').value
+    };
+
+    emailjs.send('service_vivqcds', 'template_8mkb8rn', templateParams)
+        .then(function(response) {
+            alert('Email sent successfully!');
+        }, function(error) {
+            alert('Failed to send email. Please try again later.');
+        });
+}
